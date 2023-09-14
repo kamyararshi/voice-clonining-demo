fc18f889852a1e0022eea393a591ab850d1c907e
unknown <37985246+kamyararshi@users.noreply.github.com>
2023-09-14 17:30:06 +0200

    audio playback done

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 import soundfile as sf
+import sounddevice as sd
 import base64
 import requests
 import json
@@ -30,9 +31,9 @@ def convert_chunk(wav_inputarray, sr, target_sr=22050, speaker='ahmadCorrect', a
     wav_base64 = base64.b64encode(wav)
 
     response = requests.post(f"{api_url}/convert_voice", data={"audio": wav_base64, "speaker" : speaker} )
-    print('#######################')
-    print(response)
-    print('#######################')
+    # print('#######################')
+    # print(response)
+    # print('#######################')
     response_data = json.loads(response.text)
     response_wav = base64.b64decode(response_data['audio_converted'])
     response_chunk = np.frombuffer(response_wav, dtype=np.float32)
@@ -56,6 +57,18 @@ def select_microphone():
     print(p.get_device_info_by_index(device_index))
     return device_index
 
+def list_audio_devices():
+    print("Available audio devices:")
+    devices = sd.query_devices()
+    for i, device in enumerate(devices):
+        print(f"{i}: {device['name']}")
+
+def play_audio(data, sample_rate, output_device=None):
+    # If output_device is None, it will use the default output device
+    sd.play(data, sample_rate, device=output_device)
+    sd.wait()
+
+
 def test_microphone(device_index):
     chunk_size = 1024
     sample_rate = 44100
@@ -68,7 +81,7 @@ def test_microphone(device_index):
                     output=True,
                     frames_per_buffer=chunk_size,
                     input_device_index=device_index,
-                    output_device_index=3)
+                    output_device_index=3) #set theoutput device to desired speaker or virtual mic
     
     print("Testing microphone...")
     
@@ -90,7 +103,8 @@ def record_microphone(device_index, rate, duration):
                     rate=sample_rate,
                     input=True,
                     frames_per_buffer=chunk_size,
-                    input_device_index=device_index)
+                    input_device_index=device_index, 
+                    output_device_index=3)
     
     print("Recording started...")
     
@@ -110,25 +124,40 @@ if __name__ == "__main__":
     parser.add_argument('--length', default="4")
     args = parser.parse_args()
 
+    # Test Microphone
     selected_device_index = select_microphone()
     test_microphone(selected_device_index)
+    
+    # List available audio devices
+    list_audio_devices()
+
+    # Prompt the user to select an output device by its ID (integer)
+    output_device_id = int(input("Enter the ID of the output device you want to use: "))
+
     
     target_rate = int(args.target_sr)  # Adjust this as needed
     duration = int(args.length)
     audio_generator = record_microphone(selected_device_index, target_rate, duration=duration)
     
+    chunk_size = int(target_rate*duration)
     #for _ in range(int(target_rate * duration / 44100)):  # Recording for 4 seconds
     
     filelist = [ f for f in os.listdir('tmp/') if f.endswith(".wav") ]
     for f in filelist:
         os.remove(os.path.join('tmp/', f))
+    
     i=0
     while True:
         audio_chunk = next(audio_generator)
-        sf.write(f'tmp/chunk{i}.wav', audio_chunk, 22050)
-        wav, sr = sf.read(f'tmp/chunk{i}.wav', dtype='float32')
+        #print(audio_chunk.shape, audio_chunk.dtype, type(audio_chunk))
+        #sf.write(f'tmp/chunk{i}.wav', audio_chunk, 22050)
+        #wav, sr = sf.read(f'tmp/chunk{i}.wav', dtype='float32')
+        #print(wav.shape, wav.dtype, type(wav))
         # Process the audio chunk as needed
-        #output_chunk = convert_chunk(audio_chunk, sr=target_rate, speaker=args.speaker)
-        output_chunk = convert_chunk(wav, sr=sr, speaker=args.speaker)
-        sf.write(f'tmp/output_chunk{i}.wav', output_chunk, 22050)
+        output_chunk = convert_chunk(audio_chunk.astype(np.float32), sr=target_rate, speaker=args.speaker)
+        #output_chunk = convert_chunk(wav, sr=sr, speaker=args.speaker)
+        sf.write(f'tmp/output_chunk{i}.wav', output_chunk, target_rate)
+        # data = stream_output.read(output_chunk)
+        # stream_output.write(data)
+        play_audio(output_chunk, target_rate, output_device=output_device_id)
         i += 1
